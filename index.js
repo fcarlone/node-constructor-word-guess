bd8ce713322810fb2440ca257d587cdd5f671a34
fcarlone <frankcarlone@gmail.com>
2019-04-19 16:51:34 -0400

    Handle inquirer response

--- index.js
+++ index.js
@@ -4,11 +4,53 @@ const inquirer = require('inquirer');
 const wordsArray = ['one', 'second', 'three', 'number four'];
 let newWord = '';
 
+
 const selectWord = (arr) => {
   let randomNumber = Math.floor(Math.random() * arr.length);
   console.log(randomNumber)
   newWord = arr[randomNumber];
 };
+
+const handleQuestion = () => {
+  console.log('handleQuestion function')
+  correctGuessArray = [];
+  let testArray = testWord.wordArray
+  console.log('testarray', testArray)
+
+  testArray.forEach((obj) => {
+    correctGuessArray.push(obj.correctGuess)
+  });
+  console.log(correctGuessArray)
+  if (correctGuessArray.includes(false)) {
+    console.log('run inquirer again')
+    handleInquirer()
+  } else {
+    console.log('all true... do something')
+  }
+};
+
+
+// Inquirer
+const handleInquirer = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Guess a letter",
+        name: "userLetter"
+      }
+    ])
+    .then(answers => {
+      // Use user feedback for... whatever!!
+      let userQuess = answers.userLetter;
+      console.log('Letter Guessed', userQuess)
+      testWord.checkLetter(userQuess)
+      console.log(testWord.wordArray)
+      console.log('Check response')
+      return handleQuestion()
+    });
+}
+
 
 // TEST Word
 selectWord(wordsArray)
@@ -16,27 +58,8 @@ console.log('Word to guess:', newWord)
 let testWord = new Word(newWord);
 testWord.addChars(newWord)
 console.log(testWord)
-
-
-
-// Inquirer
-inquirer
-  .prompt([
-    {
-      type: "input",
-      message: "Guess a letter",
-      name: "userLetter"
-    }
-  ])
-  .then(answers => {
-    // Use user feedback for... whatever!!
-    let userQuess = answers.userLetter;
-    console.log('Letter Guessed', userQuess)
-    testWord.checkLetter(userQuess)
-    console.log(testWord.wordArray)
-
-  });
-
+console.log('Invoke handleInquirer function');
+handleInquirer()
 
 
 
